d5857c43d756cd4849079967e29989c8304724ba
vinnypare <160244724+vinnypare@users.noreply.github.com>
2024-11-22 08:19:21 -0300

    carga_js-tp-var-fn

---
+++ type-number.js
@@ -0,0 +1,23 @@
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero - segundoNumero;
+console.log(operacaoMatematica);
+
+//ponto flutuante
+
+const numeroPontoFluente = 3.3;
+const pontoFluenteSemZero = .5;
+
+const novaOperacao = primeiroNumero / numeroPontoFLuente;
+
+console.log(novaOperacao);
+
+//Nan 
+
+const alura = "Alura";
+console.log(alura * primeiroNumero);
+
+
